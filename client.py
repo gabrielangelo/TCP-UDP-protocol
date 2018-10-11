1e83ee45a4329e7db1f0ad9c3e45de856f6659f8
gabriel <gabri005@gmail.com>
2018-10-11 12:59:37 -0300

    somes changes in client

--- client.py
+++ client.py
@@ -1,29 +1,59 @@
 import threading
 
-from protocol import TCP
+from TCP_over_UDP import TCP
+cur_ip = ""
+cur_port = ""
+cur_sock = None
 
-def send_message(msg):
-    cur_sock.send('[b]' + ('[color=%s]' % cur_color) + cur_nick + msg + '[/color]' + '[/b]')
-    print('[b]' + ('[color=%s]' % cur_color) + cur_nick + msg + '[/color]' + '[/b]')
+def connect():
+    try:
+        cur_sock = TCP()
+        cur_sock.connect((cur_ip, cur_port))
+        return cur_sock
+    except Exception as error:
+        print "An error has occured: error is:%s." % error
 
+@staticmethod
 def close_connection():
     cur_sock.close()
 
-def receiving_msg(self):
-    try:
-        while True:
-            data = cur_sock.recv()
-            if data == "Disconnected":
-                print(data) 
-                return
-            else:
-                self.ids.chat_scroll_text.text += data + "\n"
-    except Exception as error:
-        print "an error " + str(error)
+def receiving_msg1():
+    if cur_sock is not None:
+        try:
+            while True:
+                data = cur_sock.recv()
+                if data == "Disconnected":
+                    print data
+                    return
+                else:
+                    print(data)
+                    # self.ids.chat_scroll_text.text += data + "\n"
+        except Exception as error:
+            print "an error " + str(error)
+    pass
 
-def receiving_msg_handle(self):
+def receiving_msg_handle1():
     try:
-        t = threading.Thread(target=self.receiving_msg)
+        t = threading.Thread(target=receiving_msg1)
         t.start()
     except Exception as error:
-        print("an error" + str(error))
+        print "an error" + str(error)
+
+def send_message(msg):
+    cur_sock.send(msg)
+
+def run():
+    receiving_msg_handle1()
+    while True:
+        msg = input('send a message')
+        connect()
+        send_message(msg)
+        close_connection()
+
+
+        
+
+
+
+
+
